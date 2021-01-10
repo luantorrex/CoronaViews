ab876ce1b0e569f4c9a674c272ad5680162bf8bc
Luan Torres <luantorres360@gmail.com>
2021-01-09 22:16:52 -0300

    Add Cases per Day

--- app/covid_data.py
+++ app/covid_data.py
@@ -1,15 +1,41 @@
 import json
 import requests
 
+
 def get_covid_data(country):
-    data = requests.get('https://covid19.mathdro.id/api/countries/' + country)
-    data = data.json()
+    data_request = requests.get('https://covid19.mathdro.id/api/countries/' + country)
+    data_json = data_request.json()
 
-    confirmed = data['confirmed']['value']
-    recovered = data['recovered']['value']
-    deaths = data['deaths']['value']
-    last_update = data['lastUpdate'][0:10]
+    try:
+        confirmed = data_json['confirmed']['value']
+        recovered = data_json['recovered']['value']
+        deaths = data_json['deaths']['value']
+        last_update = data_json['lastUpdate'][0:10]
 
-    covid_data = [confirmed, recovered, deaths, last_update]
+        covid_data = [confirmed, recovered, deaths, last_update]
 
+    except:
+        covid_data = 'Sorry, we didnt find any data with this parameter.'
+
+    return covid_data
+
+
+def get_covid_daily_data(country_name, day):
+    data_request = requests.get('https://covid19.mathdro.id/api/daily/' + day)
+    data_json = data_request.json()
+
+    covid_data = []
+
+    for each_row in range(0, len(data_json), 1):
+        if data_json[each_row]['countryRegion'] != country_name.capitalize():
+            continue
+        else:
+            confirmed = data_json[each_row]['confirmed']
+            recovered = data_json[each_row]['recovered']
+            deaths = data_json[each_row]['deaths']
+            last_update = data_json[each_row]['lastUpdate'][0:10]
+            country = data_json[each_row]['countryRegion']
+            
+            covid_data = [confirmed, recovered, deaths, last_update, country]
+        
     return covid_data
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,52 @@
 from app import app
-from app.covid_data import get_covid_data
+from app.covid_data import (
+    get_covid_data, 
+    get_covid_daily_data)
 
 @app.route('/')
 def index():
-    return 'Welcome to CoronaViews, type a contry (in english) at the URL like this: http://127.0.0.1:5000/usa and get corona numbers about this country.'
-
-
-@app.route('/<pais>')
-def covid_data(pais):
-    data = get_covid_data(pais)
     
-    output = 'Confirmed: % s ' % data[0] + \
-        'Recovered: % s ' % data[1] + \
-        'Deaths: % s ' % data[2] + \
-        'Last Update: % s' % data[3]
+    output_phrase = 'Welcome to CoronaViews, <br><br>' + \
+    'General numbers per country: <br>' + \
+    'I.E.: <b> http://127.0.0.1:5000/usa </b> <br>' + \
+    '(Confirmed cases, Recovered people & Deaths).'
 
-    return output
+    return output_phrase
+
+
+@app.route('/country/<country>/')
+def covid_data(country):
+    cases_data = get_covid_data(country)
+
+    if isinstance(cases_data, str):
+        country_data = cases_data
+    else:
+        country_data = 'Confirmed: % s ' % cases_data[0] + '<br>' + \
+            'Recovered: % s ' % cases_data[1] + '<br>' + \
+            'Deaths: % s ' % cases_data[2] + '<br>' + \
+            'Last Update: % s' % cases_data[3]
+
+    output_phrase = country.capitalize() + '<br>' + \
+                    'General Cases' + '<br><br>' + \
+                    country_data
+
+    return output_phrase
+
+
+@app.route('/daily/<country>/<day>/')
+def daily_covid_data(country, day):
+    cases_data = get_covid_daily_data(country, day)
+
+    if len(cases_data) == 0:
+        country_data = 'Sorry, we didnt find any data with these parameters.'
+    else:
+        country_data = 'Confirmed: % s ' % cases_data[0] + '<br>' + \
+            'Recovered: % s ' % cases_data[1] + '<br>' + \
+            'Deaths: % s ' % cases_data[2] + '<br>' + \
+            'Last Update: % s' % cases_data[3]
+
+    output_phrase = country.capitalize() + '<br>' + \
+                    day + '<br><br>' + \
+                    country_data
+
+    return output_phrase
